2d37bce73deb4ae33a5b2a09920055225830d4a5
workhard1529 <117620889+workhard1529@users.noreply.github.com>
2023-03-26 05:50:10 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+  type    = string
+  default = "AKIAUYDKIU3X456VMDFS"
+}
+
+variable "aws_secret_key" {
+  type    = string
+  default = "9LGbUhyopV18Y6rkz2dRUvZRqmL3oGIjQYU1Pk1s"
+}
